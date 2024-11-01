ba7e708118e6861415195e643b66bf07121b80f0
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-01 19:46:25 +0000

    Fri, Nov 1, 2024, 7:46 PM +00:00

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 int main()
 {
     int x = 1;
-    int y = 2 * 1;
+    int y = 2;
 
     if (x < y)
     {
---
+++ test2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 2;
+    int y = 3;
+    int z = x / 3;
+    printf("%d\n", z);
+}

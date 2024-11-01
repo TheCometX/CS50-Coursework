c9e7c05a589cd982d05339654e64d0709a3bd1e8
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-01 17:39:20 +0000

    Fri, Nov 1, 2024, 5:39 PM +00:00

---
+++ compare.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int x = get_int("What is x? ");
+    int y = get_int("What is y? ");
+
+    if (x < y)
+    {
+        printf("Y is bigger\n");
+    }
+    else
+    {
+        printf("X is bigger\n");
+    }
+}

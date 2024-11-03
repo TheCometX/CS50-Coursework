d7b12cdd0637960a8e5e369849edb97f85a03fc8
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-03 21:12:39 +0000

    Sun, Nov 3, 2024, 9:12 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,25 +4,26 @@
 void stair(int z);
 int main(void)
 {
+  int height;
   do
   {
-    int height = get_int("What's the height? ");
+    height = get_int("What's the height? ");      //It will be asked until number be higher than 0
   }
   while (height < 0);
-  for (int z = 0; z < height; z++)
+  for (int z = 1; z <= height; z++)   //z start in 1 because if start in 0 the first repetition will just print a white line
     {
-    for (int x = height - z; x > 0; x--)
+    for (int x = height - z; x > 0; x--) // This loop make the spaces before #
     {
         printf(" ");
     }
     stair(z);
-    printf("  ");
+    printf("  ");   //The space between #
     stair(z);
-    printf("\n");
+    printf("\n");   //Break a line for next row start
     }
 }
 
-void stair(int z)
+void stair(int z) //It prints the # in each row, z is the number of # in row
 {
     for (int y = 0; y < z; y++)
     {

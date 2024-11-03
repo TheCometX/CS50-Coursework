cd4730b923f9afc54dccefc4916dd31f1cf39349
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-03 20:08:46 +0000

    Sun, Nov 3, 2024, 8:08 PM +00:00

--- test.c
+++ test.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void stair(z);
 int main(void)
 {
   int height = get_int("What's the height? ");
-
   for (int z = 0; z < height; z++)
     {
     for (int x = height - z; x > 0; x--)
     {
         printf(" ");
     }
-    for (int y = 0; y < z; y++)
-    {
-        printf("#");
-    }
-
+    stair(z);
     printf(" ");
+    stair(z);
+    printf("\n");
+    }
+}
 
+void stair(z)
+{
     for (int y = 0; y < z; y++)
     {
         printf("#");
-    }
-    printf("\n");
     }
 }

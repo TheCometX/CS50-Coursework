1933ee04f9c6809320b2c4366489821366c28e80
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-03 19:20:43 +0000

    Sun, Nov 3, 2024, 7:20 PM +00:00

--- test.c
+++ test.c
@@ -5,8 +5,9 @@ int main(void)
 {
     int height = get_int("What's the height");
 
-    for (int x = 1; x <= height; x++)
+    for (int x = height - 1; x > 0; x++)
     {
+
         for (int y = )
         {
         }

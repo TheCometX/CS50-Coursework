5f2611f9908d271f8f92546edd0dace8fa06959c
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-03 16:13:11 +0000

    Sun, Nov 3, 2024, 4:13 PM +00:00

--- mario.c
+++ mario.c
@@ -7,16 +7,15 @@ void stair(int x);
 int main(void)
 {
     int height = get_int("What's the height? ");
-    int num = height;
+    int times = height - 1;
 
     for (int x = 1; x <= height; x++)
     {
-        spaces(height);
+        spaces(times);
         stair(x);
         printf("\n");
-        num -= 1;
+        times -= 1;
     }
-
 }
 
 void stair(int x)
@@ -32,12 +31,10 @@ void stair(int x)
     }
 }
 
-void spaces(int h)
+void spaces(int t)
 {
-    int times = h - 1;
-    for (int z = times; z > 0; z--)
+    for (int z = t; z > 0; z--)
         {
             printf(" ");
         }
-    times -= 1;
 }

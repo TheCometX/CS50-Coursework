a07d8c8d59b57236cde7a7d8420016d3ac5ed1b2
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-03 15:19:00 +0000

    Sun, Nov 3, 2024, 3:19 PM +00:00

--- mario.c
+++ mario.c
@@ -3,8 +3,13 @@
 
 int main(void)
 {
-    for (int num = get_int("Height: \n"); num > 0; num--)
+    int num = get_int("Height: ");
+    for (int x = num; x > 0; x--)
     {
-        for (int y = num; y > 0, )
+        for (int y = num; y > 0; y--)
+        {
+            printf(" ");
+        }
+        printf("# #\n");
     }
 }

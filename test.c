f15def786cce4ce920c3f771ffa08032df283c05
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-10 22:50:10 +0000

    Sun, Nov 10, 2024, 10:50 PM +00:00

--- test.c
+++ test.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-  long cardNum = get_long("Number? \n");
+  long cardNum = get_long("Number: ");
   int length = 0;
-  for (int x = 10; x < cardNum; x * 10)
+  for (int x = 10; x < cardNum; x++)
   {
     length += 1;
-    printf("%i", length);
   }
+  printf("%i", length);
 }

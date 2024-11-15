c00672a41a582ab2fa4a655b13e9a9a4d67fac0f
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-15 17:55:02 +0000

    Fri, Nov 15, 2024, 5:55 PM +00:00

--- test.c
+++ test.c
@@ -39,6 +39,7 @@ int main() {
     }
     if (total % 10 == 0) {
     printf("Valid\n");
+    printf("%i %i\n", length, total3);
     if (length == 15 && (total2 == 7 || total2 == 10)){
         printf("American Express\n");
     }

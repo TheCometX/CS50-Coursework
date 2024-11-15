5e47634b94fa0add7499f8c151c6b703ebbd1e96
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-15 16:55:56 +0000

    Fri, Nov 15, 2024, 4:55 PM +00:00

--- scrabble.c
+++ scrabble.c
@@ -3,58 +3,19 @@
 #include <cs50.h>
 #include <ctype.h>
 
+int points(string word);
+
 int main() {
     string word1 = "Vinicius";
     string word2 = "Felipe";
     int x = 0;
-    int len1 = strlen(word1);
-    int len2 = strlen(word2);
-    int total1 = 0;
-    int total2 = 0;
-    int y = 0;
-    while (y < len1) {
-        char digit = toupper(word1[y]);
-        if (digit == 'A' || digit == 'E' || digit == 'I' || digit == 'L' || digit == 'N' || digit == 'O' || digit == 'R' || digit == 'S' || digit == 'T' || digit == 'U') {
-            total1 += 1;
-        }
-        else {
-            if (digit == 'D' || digit == 'G') {
-                total1 += 2;
-            }
-            else {
-                if (digit == 'B' || digit == 'C' || digit == 'M' || digit == 'P') {
-                total1 += 3;
-                }
-                else {
-                    if (digit == 'F' || digit == 'H' || digit == 'V' || digit == 'W' || digit == 'Y') {
-                    total1 += 4;
-                    }
-                    else {
-                        if (digit == 'K') {
-                        total1 += 5;
-                        }
-                        else {
-                            if (digit == 'J' || digit == 'X') {
-                            total1 += 8;
-                            }
-                            else {
-                                if (digit == 'Q' || digit == 'Z') {
-                                total1 += 10;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        y++;
-    }
-    printf("%i\n", total1);
+    printf("%i\n", points(word1));
 }
 
 int points(string word) {
     int total;
     int len = strlen(word);
+
     while (y < len) {
         char digit = toupper(word[y]);
         if (digit == 'A' || digit == 'E' || digit == 'I' || digit == 'L' || digit == 'N' || digit == 'O' || digit == 'R' || digit == 'S' || digit == 'T' || digit == 'U') {

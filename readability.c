cd636eebf054a09b4c7fe5d9e290a5967218eb88
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-16 20:18:39 +0000

    Sat, Nov 16, 2024, 8:18 PM +00:00

--- readability.c
+++ readability.c
@@ -19,7 +19,6 @@ int main() {
             }
         }
     }
-    int grade = 0.0588 * L - 0.296 * S - 15.8
     printf("%i\n", letters);
-    printf("%i\n", setences);
+    printf("%i\n", sentences);
 }

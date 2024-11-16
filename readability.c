0dd78e5ce6125a8997b3b9f41e0b50b59f568c38
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-16 19:50:07 +0000

    Sat, Nov 16, 2024, 7:50 PM +00:00

--- readability.c
+++ readability.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
+int main() {
+    string text = get_string("Text: ");
+    int len = strlen(text);
+    int words = 0;
+    int letters = 0;
+    for (int y = 0; y < len; y++) {
+        char letter = text[y];
+        if (strcmpr(letter, " ")) {
 
+        }
+    }
+}

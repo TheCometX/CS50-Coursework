9590c448f4d1c04138dceae709e86f20b7530033
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-18 16:50:00 +0000

    Mon, Nov 18, 2024, 4:50 PM +00:00

--- substitution.c
+++ substitution.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[]) {
     int len = strlen(argv[2]);
+    char letter[26];
     for (int y = 0; y < len; y++) {
-        char letter = 
+        char letter = (argv[2])[y];
+        if (isalpha(letter)) {
+
+        }
     }
 
     if (argc != 2) {
         printf("Usage: ./substitution key\n");
     }
     else {
-        if (strlen(len != 26) {
+        if (len != 26) {
             printf("Key must contain 26 characters.\n");
         }
     }

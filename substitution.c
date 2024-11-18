2bb426958f85bbbf03aa891e9094cefa69d48dd2
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-18 17:07:47 +0000

    Mon, Nov 18, 2024, 5:07 PM +00:00

--- substitution.c
+++ substitution.c
@@ -4,7 +4,6 @@
 #include <ctype.h>
 
 int main(int argc, string argv[]) {
-    int len = strlen(argv[2]);
     if (argc != 2) {
         printf("Usage: ./substitution key\n");
     }
@@ -13,11 +12,14 @@ int main(int argc, string argv[]) {
             printf("Key must contain 26 characters.\n");
         }
     }
+    char letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     string text = get_string("plaintext: ");
-    for (int y = 0; y < len; y++) {
+    for (int y = 0; y < 25; y++) {
         char letter = argv[y];
         if (isalpha(letter) && isdigit(letter) != false) {
-            
+            for (int x = 0; x < 25; x++) {
+                
+            }
         }
     }
 }

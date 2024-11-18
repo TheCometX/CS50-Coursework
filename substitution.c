60363ab121c28cd7293f5ce9258bc627205620df
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-18 16:54:33 +0000

    Mon, Nov 18, 2024, 4:54 PM +00:00

--- substitution.c
+++ substitution.c
@@ -5,14 +5,6 @@
 
 int main(int argc, string argv[]) {
     int len = strlen(argv[2]);
-    char letter[26];
-    for (int y = 0; y < len; y++) {
-        char letter = (argv[2])[y];
-        if (isalpha(letter)) {
-
-        }
-    }
-
     if (argc != 2) {
         printf("Usage: ./substitution key\n");
     }

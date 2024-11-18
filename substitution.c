1c3acca9623db60f17c5fc88f7f0b28963c63d2f
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-18 17:23:38 +0000

    Mon, Nov 18, 2024, 5:23 PM +00:00

--- substitution.c
+++ substitution.c
@@ -12,5 +12,6 @@ int main(int argc, string argv[]) {
             printf("Key must contain 26 characters.\n");
         }
     }
+    char letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     
 }

61f727128b103ec7864013a4b1a40958ed05739d
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-18 17:18:53 +0000

    Mon, Nov 18, 2024, 5:18 PM +00:00

--- substitution.c
+++ substitution.c
@@ -15,7 +15,7 @@ int main(int argc, string argv[]) {
     char letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     string text = get_string("plaintext: ");
     for (int y = 0; y < 25; y++) {
-        char letter = argv[y];
+        char letter = (argv[2])[y];
         if (isalpha(letter) && isdigit(letter) != false) {
             for (int x = 0; x < 25; x++) {
                 char letter2 = letters[x];

a9eae30f243e7fa4a66453cbe5a7e05e7de6e9d3
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-11-23 19:32:05 +0000

    Sat, Nov 23, 2024, 7:32 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int main(int argc, char *argv[])
 {
- if (argc != 4)
- {
-    printf("Usage: ./volume input.wav output.wav factor\n");
- }
- FILE *input = fopen(argv[1], "r");
- if (input == NULL)
- {
-    printf("Could not open file.\n");
-    return 1;
- }
- FILE *output = fopen(argv[2], "w");
- if (output == NULL)
- {
-    printf("Could not open file.\n");
-    return 1;
- }
 
 }
-
-int check(FILE )

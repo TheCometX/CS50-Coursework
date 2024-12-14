830cedbe4db9791caaf7fe875b15d980503e178a
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 23:48:58 +0000

    Sat, Dec 14, 2024, 11:48 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -22,7 +22,11 @@ int main(int argc, string argv[]) {
             printf("Key must contain only letters\n");
             return 1;
         }
-        else if ()
+        else if (strchar(key) != strrchar(key))
+        {
+            printf("Repeating character\n");
+            return 1;
+        }
     }
 
     char letters[26]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};

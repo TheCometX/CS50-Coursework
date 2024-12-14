119a87ee5a18d72bbd14a5f0b601a9acb4b8f81b
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 23:38:24 +0000

    Sat, Dec 14, 2024, 11:38 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -14,7 +14,16 @@ int main(int argc, string argv[]) {
         return 1;
     }
 
-    for (int z = 0; z )
+    for (int z = 0; z < 26; z++)
+    {
+        char key = argv[1][z];
+        if (!isalpha(key))
+        {
+            printf("Key must contain only letters\n");
+            return 1;
+        }
+        else if ()
+    }
 
     char letters[26]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     string text = get_string("plaintext: ");

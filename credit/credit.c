874f672f21699b3d9df210e0e65d0f38bc00dc5d
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 15:17:13 +0000

    Sat, Dec 14, 2024, 3:17 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,9 +4,11 @@
 
 int main() {
     long int cardNum = get_long("Number: ");
+    int length = 0;
 
     for (long int x = 1; x <= cardNum; pow(x, 10))
     {
-        if 
+        length += 1;
+        if (
     }
 }

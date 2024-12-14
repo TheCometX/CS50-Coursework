b30dd5606fb909a8c83197bb5e457be1804ec096
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 16:26:02 +0000

    Sat, Dec 14, 2024, 4:26 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -18,7 +18,7 @@ int main() {
             printf("%i\n", digit);
             int digit2 = digit * 2;
             result += digit2 % 10;
-            result += digit2 / 10;
+            result += digit2 / 1;
         }
         else
         {

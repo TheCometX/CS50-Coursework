5288a17aeaa12f9802347b56b6ba0a9a41e66458
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 23:34:13 +0000

    Sat, Dec 14, 2024, 11:34 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -14,6 +14,8 @@ int main(int argc, string argv[]) {
         return 1;
     }
 
+    for (int z = 0; z )
+
     char letters[26]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     string text = get_string("plaintext: ");
 

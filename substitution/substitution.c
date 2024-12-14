ddaa3d3bc688f0970f92bcf16b8d0de65c66b721
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 23:02:11 +0000

    Sat, Dec 14, 2024, 11:02 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -17,5 +17,5 @@ int main(int argc, string argv[]) {
     char letters[26]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     string text = get_string("plaintext: ");
 
-    
+    for (int x = 0; x < strlen(text); x++)
 }

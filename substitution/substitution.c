40971f25fae31f45ee5c0d8ec6e65d0fbbb7c853
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 22:56:44 +0000

    Sat, Dec 14, 2024, 10:56 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,13 +8,14 @@ int main(int argc, string argv[]) {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    else {
-        if (strlen(argv[1]) != 26) {
-            printf("Key must contain 26 characters.\n");
-        }
-        else {
-            char letters[26]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-            string text = get_string("plaintext: ");
+
+    if (strlen(argv[1]) != 26) {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
+    char letters[26]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    string text = get_string("plaintext: ");
             for (int z = 0; z < strlen(text); z++) {
                 text[z] = tolower(text[z]);
             }

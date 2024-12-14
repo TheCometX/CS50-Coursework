2856484b4f8977482a4f2b5089be9d899595f0b6
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 23:18:26 +0000

    Sat, Dec 14, 2024, 11:18 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -19,16 +19,24 @@ int main(int argc, string argv[]) {
 
     for (int x = 0; x < strlen(text); x++)
     {
-        char letter = text[x];
+        char letter = tolower(text[x]);
         for (int y = 0; y < 26; y++)
         {
             char alpha = letters[y];
             if (letter == alpha)
             {
-                text[x] = argv[1][y];
+                if (x == 0)
+                {
+                    text[x] = toupper(argv[1][y]);
+                }
+                else
+                {
+                    text[x] = argv[1][y];
+                }
             }
         }
     }
 
     printf("%s\n", text);
+    return 0;
 }

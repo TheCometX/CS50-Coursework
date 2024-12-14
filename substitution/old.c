0795ad933281cb6d7de276b2320cca9e3b1410b0
ViniFerrari09 <viniciusssferrari@gmail.com>
2024-12-14 22:39:39 +0000

    Sat, Dec 14, 2024, 10:39 PM +00:00

---
+++ substitution/old.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[]) {
+    if (argc != 2) {
+        printf("Usage: ./substitution key\n");
+    }
+    else {
+        if (strlen(argv[1]) != 26) {
+            printf("Key must contain 26 characters.\n");
+        }
+        else {
+            char letters[26]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+            string text = get_string("plaintext: ");
+            for (int z = 0; z < strlen(text); z++) {
+                text[z] = tolower(text[z]);
+            }
+            for (int x = 0; x < strlen(text); x++) {
+                char letter = text[x];
+                if (isalpha(letter)) {
+                    for (int y = 0; y < 26; y++) {
+                        if (letter == letters[y]) {
+                            char letter2 = argv[1][y];
+                            if (x == 0) {
+                                printf("%c", toupper(letter2));
+                            }
+                            else {
+                                printf("%c", tolower(letter2));
+                            }
+                        }
+                    }
+                }
+                else {
+                    printf("%c", letter);
+                }
+            }
+            printf("\n");
+        }
+    }
+}

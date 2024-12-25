5ebfa4a06b9323a3329ec45d9d74fd9e125a07ec
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-25 19:37:24 +0000

    Wed, Dec 25, 2024, 7:37 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,8 +52,10 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] = 
-        new_person->alleles[1] =
+        int index1 = rand() % 2;
+        new_person->alleles[0] = new_person->parents[0]->alleles[index1];
+        int index2 = rand() % 2;
+        new_person->alleles[1] = new_person->parents[1]->alleles[index2];
     }
 
     // If there are no generations left to create

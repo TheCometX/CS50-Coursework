dbdbd86addd2f956b630da55ffba67bb6f4c62cd
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-25 22:27:50 +0000

    Wed, Dec 25, 2024, 10:27 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,7 +36,11 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     FILE* dict = fopen(dictionary, "r");
-    
+    if (dict == NULL)
+    {
+        printf("Cannot open the file\n");
+        return false;
+    }
     return false;
 }
 

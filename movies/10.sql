a8c13f8e9805a3c58ca1750387aca5f8c51c62b6
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-28 19:12:02 +0000

    Sat, Dec 28, 2024, 7:12 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT(name) FROM people, directors, ratings WHERE people.id = person_id AND directors.movie_id = ratings.movie_id AND rating > 9.0;
+SELECT DISTINCT(name) FROM people, directors, ratings WHERE people.id = person_id AND directors.movie_id = ratings.movie_id AND rating > 9;

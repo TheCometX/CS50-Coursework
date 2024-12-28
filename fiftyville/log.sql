0d605b5fa8e30bc48314cb9d9fa569714afb2b74
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-28 15:15:13 +0000

    Sat, Dec 28, 2024, 3:15 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -32,3 +32,26 @@ AND month = 7 AND day = 28 AND hour = 10 AND minute < 25 AND minute > 05;
 26013199 */
 SELECT account_number, amount FROM atm_transactions WHERE year = 2023 AND month = 7
 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
+
+/* Owners of plates (suspects):
++---------+----------------+-----------------+
+|  name   |  phone_number  | passport_number |
++---------+----------------+-----------------+
+| Vanessa | (725) 555-4692 | 2963008352      |
+| Barry   | (301) 555-4174 | 7526138472      |
+| Brandon | (771) 555-6667 | 7874488539      |
+| Iman    | (829) 555-5269 | 7049073643      |
+| Sofia   | (130) 555-0289 | 1695452385      |
+| Luca    | (389) 555-5198 | 8496433585      |
+| Diana   | (770) 555-1861 | 3592750733      |
+| Kelsey  | (499) 555-9472 | 8294398571      |
+| Bruce   | (367) 555-5533 | 5773159633      |
+| Sophia  | (027) 555-1068 | 3642612721      |
++---------+----------------+-----------------+ */
+SELECT name, phone_number, passport_number FROM people WHERE license_plate IN (SELECT license_plate
+FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute < 25 AND minute > 05);
+
+
+
+SELECT name, phone_number, passport_number FROM people WHERE license_plate IN (SELECT license_plate
+FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute < 25 AND minute > 05);

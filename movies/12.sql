d0dbd58b326afa5853ce414442c552944835ae98
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-28 18:27:20 +0000

    Sat, Dec 28, 2024, 6:27 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,2 @@
 SELECT title FROM movies, stars, people WHERE movies.id = movie_id
-AND person_id = people.id AND name = 'Bradley Cooper' AND name = 'Jennifer Lawrence';
+AND name = 'Jennifer Lawrence' AND title IN ()

1d2af5409de308ba8de21b3f52afe0a183e75c52
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-28 16:30:50 +0000

    Sat, Dec 28, 2024, 4:30 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -41,5 +41,17 @@ SELECT name, license_plate FROM people WHERE license_plate IN (SELECT license_pl
 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = 'exit');
 
 /* atm transactions:
-*/
-SELECT account_number FROM atm_transactions WHERE 
++----------------+
+| account_number |
++----------------+
+| 28500762       |
+| 28296815       |
+| 76054385       |
+| 49610011       |
+| 16153065       |
+| 25506511       |
+| 81061156       |
+| 26013199       |
++----------------+ */
+SELECT account_number FROM atm_transactions WHERE year = 2023 AND month = 7
+AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';

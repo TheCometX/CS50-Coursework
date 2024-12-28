d54534dd444b62423d8f6896d98a1ff64bf1d227
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-28 16:20:12 +0000

    Sat, Dec 28, 2024, 4:20 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,20 @@ SELECT id, street, description FROM crime_scene_reports WHERE day = 28 AND month
 SELECT name, transcript FROM interviews WHERE year = 2023 AND month = 7 AND day = 28;
 
 /* license plates:
++---------------+
+| license_plate |
++---------------+
+| 5P2BI95       |
+| 94KL13X       |
+| 6P58WS2       |
+| 4328GD8       |
+| G412CB7       |
+| L93JTIZ       |
+| 322W7JE       |
+| 0NTHK55       |
++---------------+ */
+SELECT license_plate FROM bakery_security_logs WHERE year = 2023 AND month = 7
+AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = 'exit';
+
+/* plates owner:
+*/

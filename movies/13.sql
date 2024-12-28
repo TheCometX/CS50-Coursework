ba5530ba2fc6d7af208d3a3902e9692cc65dd993
Vinicius Ferrari <viniciusssferrari@gmail.com>
2024-12-28 18:59:07 +0000

    Sat, Dec 28, 2024, 6:59 PM +00:00

---
+++ movies/13.sql
@@ -0,0 +1,3 @@
+SELECT name FROM movies, stars, people WHERE movies.id = movie_id AND person_id = people.id
+AND title IN (SELECT title FROM movies, stars, people WHERE movies.id = movie_id AND person_id = people.id
+AND name = 'Kevin Bacon');

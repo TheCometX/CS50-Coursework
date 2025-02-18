fc0d8cad6cf296f56a6888838f5e10b6540c4244
Vinicius Ferrari <viniciusssferrari@gmail.com>
2025-02-18 19:17:00 +0000

    Tue, Feb 18, 2025, 7:17 PM +00:00

--- project/server.py
+++ project/server.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def homepage():
     return render_template("homepage.html")
+
+@app.route("/test")
+def test():
+    return render_template("questions.html")
 
 

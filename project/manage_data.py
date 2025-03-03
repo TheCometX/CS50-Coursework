882c09362aa0b1729134f6656970dcc85f9a4e69
Vinicius Ferrari <viniciusssferrari@gmail.com>
2025-03-03 18:00:50 +0000

    Mon, Mar 3, 2025, 6:00 PM +00:00

--- project/manage_data.py
+++ project/manage_data.py
@@ -6,7 +6,7 @@ class Questions:
     def add_questions():
         database = sqlite3.connect('')
 
-with open('https://opentdb.com/api.php?amount=50&type=multiple', 'r') as file:
+with open('trivia_api.json', 'r') as file:
     data = json.load(file)
     for x in data['results']:
         print(x)

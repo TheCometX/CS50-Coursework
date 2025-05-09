7f256ebfacf8a8a8080f5fe1fb332075cc920c91
Vinicius Ferrari <viniciusssferrari@gmail.com>
2025-05-09 17:05:32 +0000

    Fri, May 9, 2025, 6:05 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,25 +1,99 @@
 #include "helpers.h"
+#include <stdio.h>
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int x = 0; x < height; x++) {
+        for (int y = 0; y < width; y++) {
+            int grey = round((image[x][y].rgbtBlue + image[x][y].rgbtGreen + image[x][y].rgbtRed) / 3.0);
+            image[x][y].rgbtBlue = image[x][y].rgbtGreen = image[x][y].rgbtRed = grey;
+        }
+    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int x = 0; x < height; x++) {
+        for (int y = 0; y < width; y++) {
+            int newBlue = round(0.272 * image[x][y].rgbtRed + 0.534 * image[x][y].rgbtGreen + 0.131 * image[x][y].rgbtBlue);
+            int newGreen = round(0.349 * image[x][y].rgbtRed + 0.686 * image[x][y].rgbtGreen + 0.168 * image[x][y].rgbtBlue);
+            int newRed = round(0.393 * image[x][y].rgbtRed + 0.769 * image[x][y].rgbtGreen + 0.189 * image[x][y].rgbtBlue);
+            if (newBlue > 255) {
+                image[x][y].rgbtBlue = 255;
+            }
+            else {
+                image[x][y].rgbtBlue = newBlue;
+            }
+            if (newGreen > 255) {
+                image[x][y].rgbtGreen = 255;
+            }
+            else {
+                image[x][y].rgbtGreen = newGreen;
+            }
+            if (newRed > 255) {
+                image[x][y].rgbtRed = 255;
+            }
+            else {
+                image[x][y].rgbtRed = newRed;
+            }
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int x = 0; x < height; x++) {
+        float halfWidth = width / 2.0;
+        for (int y = 0; y < halfWidth; y++) {
+            RGBTRIPLE temp = image[x][y];
+            image[x][y] = image[x][width - 1 - y];
+            image[x][width - 1 - y] = temp;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[height][width];
+    for (int x = 0; x < height; x++) {
+        for (int y = 0; y < width; y++) {
+            float totalBlue = 0;
+            float totalGreen = 0;
+            float totalRed = 0;
+            float pixels = 0.0;
+            for (int z = x - 1; z < x + 2; z++) {
+                if (z > height - 1 || z < 0) {
+                    continue;
+                }
+                for (int c = y - 1; c < y + 2; c++) {
+                    if (c < 0 || c > width - 1) {
+                        continue;
+                    }
+                    totalBlue += image[z][c].rgbtBlue;
+                    totalGreen += image[z][c].rgbtGreen;
+                    totalRed += image[z][c].rgbtRed;
+                    pixels++;
+                }
+            }
+            temp[x][y].rgbtRed = round(totalRed / pixels);
+            temp[x][y].rgbtGreen = round(totalGreen / pixels);
+            temp[x][y].rgbtBlue = round(totalBlue / pixels);
+        }
+    }
+    for (int s = 0; s < height; s++) {
+        for (int g = 0; g < width; g++) {
+            image[s][g].rgbtBlue = temp[s][g].rgbtBlue;
+            image[s][g].rgbtGreen = temp[s][g].rgbtGreen;
+            image[s][g].rgbtRed = temp[s][g].rgbtRed;
+        }
+    }
     return;
 }
--- finance/app.py
+++ finance/app.py
@@ -22,7 +22,6 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -35,46 +34,55 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    stocks = db.execute("SELECT symbol, COUNT(shares), SUM(price) FROM history WHERE userID = ? GROUP BY symbol", session.get("user_id"))
-    balance = db.execute("SELECT cash FROM users WHERE id = ?", session.get("user_id"))[0]["cash"]
-    total = 0
-    for row in stocks:
-        total += row["SUM(price)"]
-        row["stockprice"] = lookup(row["symbol"])["price"]
-    return render_template("index.html", stocks=stocks, balance=balance, total=total)
-
+    id = session.get("user_id")
+    information = db.execute("SELECT symbol, amount FROM stocks WHERE userID = ?", id)
+    balance = db.execute("SELECT cash FROM users WHERE id = ?", id)[0]["cash"]
+    total = balance
+    for row in information:
+        symbol = row["symbol"]
+        row["price"] = lookup(symbol)["price"]
+        row["total"] = round(row["price"] * row["amount"], 2)
+        total += row["total"]
+    return render_template("index.html", balance=round(balance, 2), stocks=information, total=round(total, 2))
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     if request.method == "POST":
         symbol = request.form.get("symbol")
-        information = lookup(symbol)
-        if information is not None:
-            try:
-                shares = int(request.form.get("shares"))
-                price = information["price"] * shares
-                userID = session.get("user_id")
-                balance = db.execute("SELECT cash FROM users WHERE id = ?", userID)[0]["cash"]
-                if balance > price:
-                    balance = round(balance - price, 2)
-                    dateTime = datetime.now()
-                    db.execute("UPDATE users SET cash = ? WHERE id = ?", balance, userID)
-                    db.execute("INSERT INTO history(shares, price, date, userID, symbol) VALUES (?, ?, ?, ?, ?)", shares, price, dateTime, userID, symbol)
-                    return redirect("/")
-            except ValueError:
-                return apology("Invalid number of shares")
+        shares = request.form.get("shares")
+        if symbol == "":
+            return apology("Input a symbol")
+        try:
+            shares = float(shares)
+        except ValueError:
+            return apology("Shares should be a number")
+        if shares < 0 or shares % 1 != 0:
+            return apology("Shares should be a integer higher than 0")
+        stock = lookup(symbol)
+        if stock == None:
+            return apology("Stock symbol doesn't exist")
+        price = round(stock["price"] * shares, 2)
+        id = session.get("user_id")
+        balance = db.execute("SELECT cash FROM users WHERE id = ?", id)[0]["cash"]
+        if balance < price:
+            return apology("Your balance isn't enough")
+        db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", price, id)
+        dateTime = datetime.now()
+        db.execute("INSERT INTO history(date, type, price, stockSymbol, userID, shares) VALUES (?, 'buy', ?, ?, ?, ?)", dateTime, price, symbol, id, shares)
+        stocksOwen = db.execute("SELECT amount FROM stocks WHERE userID = ? AND symbol = ?", id, symbol)
+        if len(stocksOwen) == 0:
+            db.execute("INSERT INTO stocks(symbol, amount, userID) VALUES (?, ?, ?)", symbol, shares, id)
         else:
-            return apology("Invalid stock's symbol")
+            db.execute("UPDATE stocks SET amount = amount + ? WHERE userID = ? AND symbol = ?", shares, id, symbol)
+        return redirect("/")
     return render_template("buy.html")
-
 
 @app.route("/history")
 @login_required
 def history():
-    """Show history of transactions"""
-    return apology("TODO")
-
+    transactions = db.execute("SELECT date, type, price, stockSymbol, shares FROM history WHERE userID = ? ORDER BY date DESC", session.get("user_id"))
+    return render_template("history.html", transactions=transactions)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -131,35 +139,56 @@ def logout():
 def quote():
     if request.method == "POST":
         symbol = request.form.get("symbol")
-        informations = lookup(symbol)
-        name = informations["name"]
-        price = informations["price"]
-        return render_template("quoted.html", symbol=symbol, name=name, price=price)
+        information = lookup(symbol)
+        if information != None:
+            return render_template("quoted.html", symbol=symbol, company=information["name"], price=round(information["price"]))
+        return apology("Invalid symbol input")
     return render_template("quote.html")
-
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
     if request.method == "POST":
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
-        if confirmation == password and (confirmation != "" and password != ""):
+        if confirmation == password and confirmation != "" and password != "":
             username = request.form.get("username")
-            hash = generate_password_hash(password)
             if username != "":
+                hash = generate_password_hash(password)
                 try:
                     db.execute("INSERT INTO users(username, hash) VALUES (?, ?)", username, hash)
+                    return redirect("/login")
                 except ValueError:
-                    return apology("Username in use")
-            else:
-                return apology("Invalid username input")
-        else:
-            return apology("Invalid password input")
+                    return apology("Username already exist")
+            return apology("Invalid username input")
+        return apology("Invalid password input")
     return render_template("register.html")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
-    """Sell shares of stock"""
-    return apology("TODO")
+    id = session.get("user_id")
+    symbols = db.execute("SELECT symbol FROM stocks WHERE userID = ?", id)
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        stock = lookup(symbol)
+        if stock != None:
+            shares = request.form.get("shares")
+            try:
+                shares = int(float(shares))
+                if shares > 0:
+                    sharesOwened = db.execute("SELECT amount FROM stocks WHERE symbol = ?", symbol)[0]["amount"]
+                    if sharesOwened >= shares:
+                        price = round(stock["price"] * shares, 2)
+                        dateTime = datetime.now()
+                        if sharesOwened != shares:
+                            db.execute("UPDATE stocks SET amount = amount - ? WHERE userID = ? AND symbol = ?", shares, id, symbol)
+                        else:
+                            db.execute("DELETE FROM stocks WHERE userID = ? AND symbol = ?", id, symbol)
+                        db.execute("UPDATE users SET cash = cash + ? WHERE id = ?", price, id)
+                        db.execute("INSERT INTO history(date, type, price, stockSymbol, userID, shares) VALUES (?, 'sell', ?, ?, ?, ?)", dateTime, price, symbol, id, shares)
+                        return redirect("/")
+            except ValueError:
+                pass
+            return apology("Invalid shares input", 4004)
+        return apology("Invalid symbol input")
+    return render_template("sell.html", symbols=symbols)

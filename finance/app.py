9fcf3f670e51576396855c5c72bdb25867820034
Vinicius Ferrari <viniciusssferrari@gmail.com>
2025-05-22 17:29:48 +0000

    Thu, May 22, 2025, 6:29 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -64,7 +64,7 @@ def buy():
             return apology("Don't have enough money")
         dateTime = datetime.now()
         db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", price, session["user_id"])
-        db.execute("INSERT INTO history(userID, type, stockSymbol, price, shares, dateTime) VALUES (?, "Buy", ?, ?, ?, ?)", session["user_id"], stock["symbol"], price, shares, dateTime)
+        db.execute("INSERT INTO history(userID, type, stockSymbol, price, shares, dateTime) VALUES (?, 'Buy', ?, ?, ?, ?)", session["user_id"], stock["symbol"], price, shares, dateTime)
         update = db.execute("UPDATE portfolio SET shares = shares + ? WHERE userID = ? AND stockSymbol = ?", shares, session["user_id"], stock["symbol"])
         if update == 0:
             db.execute("INSERT INTO portfolio(userID, shares, stockSymbol) VALUES (?, ?, ?)", session["user_id"], shares, stock["symbol"])

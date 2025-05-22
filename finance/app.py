655f0700f01ff66a3e02c183b0067bed696f8a95
Vinicius Ferrari <viniciusssferrari@gmail.com>
2025-05-22 16:16:29 +0000

    Thu, May 22, 2025, 5:16 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -111,7 +111,7 @@ def quote():
         stock = lookup(symbol)
         if stock == None:
             return apology("Invalid symbol")
-        return render_template("quoted.html", company=stock["name"], symbol=stock["symbol"], price=stock["price"])
+        return render_template("quoted.html", name=stock["name"], symbol=stock["symbol"], price=stock["price"])
     return render_template("quote.html")
 
 
